7f8b1d1c404010504d80c92f9e82223749f3a0c7
milos.peric <steamestt@gmail.com>
2020-06-19 15:28:48 +0200

    + Add project files

---
+++ DAN_XXXV_MilosPeric/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XXXV_MilosPeric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
